3c6e67dfb0692dc500319628107d6f89f1b2dc5c
Jeferson Soares <jpadilha53@gmail.com>
2024-08-12 17:58:35 -0300

    criacao do componente botao

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,16 +1,28 @@
 import { Image, Titulo, Precos } from './styles'
 
 import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => {
   return (
     <Image style={{ backgroundImage: `url(${bannerImg})` }}>
       <div className="container">
-        <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
-        <Precos>
-          <span>De R$ 250,00</span> <br />
-          por apenas R$ 99,90
-        </Precos>
+        <div>
+          <Tag size="big">Destaque do dia</Tag>
+          <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
+          <Precos>
+            <span>De R$ 250,00</span> <br />
+            por apenas R$ 99,90
+          </Precos>
+        </div>
+        <Button
+          type="link"
+          to="/produto"
+          title="Clique aqui para aproveitar essa oferta"
+        >
+          Aproveitar
+        </Button>
       </div>
     </Image>
   )
